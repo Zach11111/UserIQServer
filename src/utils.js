f6559df34a047ebc46db47148f63b02403e596fb
zach orton <zacrut23@gmail.com>
2025-03-03 22:35:06 -0600

    Add Discord OAuth2 using the functions in db.js to create a user and store the token

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
+const fetch = require('node-fetch');
 
 async function doesUserIDExist(userId) {
     const url = `https://discord.com/api/v10/users/${userId}`;
@@ -17,6 +18,24 @@ async function doesUserIDExist(userId) {
     } catch (err) {
       console.error('Error:', err);
     }
-  }
+}
 
-  module.exports = { doesUserIDExist };
+async function getDiscordUser(accessToken) {
+    const url = 'https://discord.com/api/v10/users/@me';
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+    };
+
+    try {
+        const res = await fetch(url, { method: 'GET', headers: headers });
+        if (res.ok) {
+            return await res.json();
+        } else {
+            throw new Error('Failed to fetch user data from Discord');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+module.exports = { doesUserIDExist, getDiscordUser };
